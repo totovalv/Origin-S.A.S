142ed86c71e89d985df391f90787885eee8beec5
originholdingssas <origin.holdings.sas@gmail.com>
2025-07-31 14:24:30 -0500

    correccion del error anterior y eliminacion de span algo se jod

--- src/components/Proyects/Origin3/Origin3.jsx
+++ src/components/Proyects/Origin3/Origin3.jsx
@@ -812,6 +812,8 @@ function Origin3() {
         </div>
       </div>  
       
+
+      <span>Algo se jod</span>
       <div className={style.timeline}>
         <img className={style.imgTimeline} src={data.timeline} alt="timeline" />
       </div>
